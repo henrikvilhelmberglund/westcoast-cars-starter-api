d15b727851844f5770e5a4ea941c164dbeb03f0b
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-11-10 10:31:14 +0100

    feat: update

--- server.mjs
+++ server.mjs
@@ -1,18 +1,30 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-import { listAllVehicles } from './mock/vehicles.mjs';
+import { getVehicle, listAllVehicles } from "./mock/vehicles.mjs";
 
 const app = express();
 
 app.use(cors());
 
-app.get('/api/', (req, res) => {
+app.get("/api/", (req, res) => {
   const vehicles = listAllVehicles();
+  res.status(200).json({
+    status: "Success",
+    statusCode: 200,
+    items: vehicles.length,
+    data: vehicles,
+  });
+});
+
+app.get("/api/:id", (req, res) => {
+  const vehicle = getVehicle(+req.params.id);
   res
     .status(200)
-    .json({ status: 'Success', statusCode: 200, items: vehicles.length, data: vehicles });
+    .json({ status: "success", statusCode: 200, data: vehicle ?? null });
 });
 
 const port = 3000;
-app.listen(port, () => console.log(`Server is up and running on port: ${port}`));
+app.listen(port, () =>
+  console.log(`Server is up and running on port: ${port}`)
+);
